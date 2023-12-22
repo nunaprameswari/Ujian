4876c7f8a401ec893e53b2e5e359d7e0c1899744
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-22 20:03:42 +0700

    mengisi bagian model

--- army/models.py
+++ army/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+    Boutique = models.ForeignKey(Boutiq, on_delete=models.CASCADE)
+    judul = models.CharField(max_length=100)
+    isi_Tokoku = models.TextField()
+    
+    def __str__(self):
+        return f"{self.judul} {self.isi_Tokoku}"
