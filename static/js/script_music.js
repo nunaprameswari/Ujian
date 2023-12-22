276f4f5a29c3fe12e9f27ebb8a46178e327b9131
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-22 12:02:26 +0700

    mengaktifkan search_rsult di script

--- static/js/script_music.js
+++ static/js/script_music.js
@@ -193,6 +193,25 @@ songs.forEach(element => {
     `;
     search_result.appendChild(card);
 });
+
+
+let input = document.getElementsByTagName('input')[0];
+
+input.addEventListener('keyup', () => {
+    let input_value = input.value.toUpperCase();
+    let items = search_result.getElementsByTagName('a');
+
+    for (let index = 0; index < items.length; index++) {
+        let as = items[index].getElementsByClassName('content')[0];
+        let text_value = as.textContent || as.innerHTML;
+        
+        if (text_value.toUpperCase().indexOf(input_value) > -1) {
+            items[index].style.display = "flex";
+        } else {
+            items[index].style.display = "none";
+        }
+    }
+})
 // search data end
 
 
