bed01d4ab95e678609f9746cfc479cbc317a23dc
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-22 20:01:24 +0700

    mengisi admin

--- army/admin.py
+++ army/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Artist
 
 # Register your models here.
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ("judul", "isi_Tokoku",)
+
+admin.site.register(Tokoku, TokokuAdmin)
