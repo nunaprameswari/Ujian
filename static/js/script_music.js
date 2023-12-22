30dadb887cd6008f87f6156ded6385faa613f4a1
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-22 16:02:16 +0700

    mengalami perubahan

--- static/js/script_music.js
+++ static/js/script_music.js
@@ -572,20 +572,3 @@ music.addEventListener('ended', () => {
             break;
     }
 })
-
-
-let menu_list_active_button = document.getElementById('menu_list_active_button');
-let menu_side = document.getElementsByClassName('menu_side')[0];
-
-menu_list_active_button.addEventListener('click', () => {
-menu_side.style.transform = "unset";
-menu_list_active_button.style.opacity = 0;
-})
-
-
-let song_side = document.getElementsByClassName('song_side')[0];
-
-song_side.addEventListener('click', () => {
-    menu_side.style.transform = "translateX(-100%)";
-    menu_list_active_button.style.opacity = 1;
-})
