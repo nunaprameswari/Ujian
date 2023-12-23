e0e18a1f71b57a63004a2f761012582bc66498e8
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-23 10:31:00 +0700

    penambahan admin

--- army/admin.py
+++ army/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Artist
+from .models import Artist, Idol
 
 # Register your models here.
 
 class ArtistAdmin(admin.ModelAdmin):
-    list_display = ("judul", "isi_Tokoku",)
+    list_display = ("Nama_Panggung", "Tanggal_Lahir", "Alamat", "Pesan_Moral",)
 
 admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Idol)
