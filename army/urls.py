633807626cf3f264054155527d07d3b198342a26
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-23 11:07:41 +0700

    mengalami perubahan

--- army/urls.py
+++ army/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('music', views.music, name='music'),
     path('seokjin', views.seokjin, name='seokjin'),
     path('Idol', views.Idol, name='Idol'),
-    path('Artist', views.Artist, name='artist'),
+    path('artist', views.artist, name='artist'),
 ]
