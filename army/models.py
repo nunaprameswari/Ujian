6ea96ec4a5c6b811685698e85f85187ed6443e1f
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-23 10:27:54 +0700

    penambahan model

--- army/models.py
+++ army/models.py
@@ -2,9 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class Idol(models.Model):
+    Nama_Idol = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.Nama_Idol}"
+
 class Artist(models.Model):
-    judul = models.CharField(max_length=100)
-    isi_Tokoku = models.TextField()
+    Idola = models.ForeignKey(Idol, on_delete=models.CASCADE)
+    Nama_Panggung = models.CharField(max_length=100)
+    Tanggal_Lahir = models.CharField(max_length=100)
+    Alamat = models.CharField(max_length=100)
+    Pesan_Moral = models.TextField()
     
     def __str__(self):
-        return f"{self.judul} {self.isi_Tokoku}"
+        return f"{self.Nama_Panggung} {self.Tanggal_Lahir} {self.Alamat} {self.Pesan_Moral}"
