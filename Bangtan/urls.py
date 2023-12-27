789647c25d35f0fc93dbfa83054de6bd22c2ce85
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-27 21:09:39 +0700

    mengedit tampilan login

--- Bangtan/urls.py
+++ Bangtan/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from .views import login
+from .views import login, signup
 
 urlpatterns = [
     path('', login, name='login'),
+    path('signup', signup, name='signup'),
     path('', include('army.urls')),
     path('admin/', admin.site.urls),
 ]
--- Bangtan/views.py
+++ Bangtan/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 def login(request):
     template = loader.get_template('login.html')
     return HttpResponse(template.render())
+
+def signup(request):
+    template = loader.get_template('signup.html')
+    return HttpResponse(template.render())
