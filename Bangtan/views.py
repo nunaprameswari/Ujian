16045b1135b825099cac0b36988f89436df652d0
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-27 14:31:19 +0700

    mencoba buat user login

--- Bangtan/views.py
+++ Bangtan/views.py
@@ -1,9 +1,26 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 def menu(request):
     context = {
         'page_title': 'Home',
     }
+    
     return render(request, 'menu.html', context)
+
+def loginView(request):
+    context = {
+        'page_title': 'LOGIN',
+    }
+    
+    username_nindia = 'admin'
+    password_nindia = 'admin123'
+
+    user = authenticate(request, username=username_nindia, password=password_nindia)
+    print(user)
+
+    login(request, user)
+
+    return render(request, 'login.html', context)
