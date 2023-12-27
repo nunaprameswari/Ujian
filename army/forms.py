ab89432ec03b3e2c6aac1d2f8de099bbbbe266a0
nunaprameswari <nuna.prameswari0930@gmail.com>
2023-12-27 18:22:15 +0700

    mencoba buat user login

--- army/forms.py
+++ army/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from django.contrib.auth.models import Instagram, User
 
-from .models import Instagram
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget = form.TextInput(attrs('class':'form-control', 'type':'text')),
+    )
+    password = form.CharField(
+        widget = form.PasswordInput(attrs('class':'form-control'))
+    )
 
 class InstagramForm(forms.ModelForm):
     class Meta:
