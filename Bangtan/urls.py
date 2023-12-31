623c6b90745b0bcd3c959f3015eb6aae04eb7608
nindiappc <nindi.nindia44@gmail.com>
2023-12-31 19:29:36 +0700

    pembuatan upload video

--- Bangtan/urls.py
+++ Bangtan/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import music
 
 urlpatterns = [
@@ -23,3 +25,5 @@ urlpatterns = [
     path('', include('army.urls')),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
