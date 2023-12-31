0d3869939ffa157201bae5e53a3cb847f0774080
nindiappc <nindi.nindia44@gmail.com>
2023-12-31 20:17:12 +0700

    pembuatan upload image

--- Bangtan/urls.py
+++ Bangtan/urls.py
@@ -24,6 +24,4 @@ urlpatterns = [
     path('', music, name='music'),
     path('', include('army.urls')),
     path('admin/', admin.site.urls),
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- army/admin.py
+++ army/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from .models import Artist, Idol, Video
+from .models import Artist, Idol, Image
 
 # Register your models here.
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'photo', 'date']
 
 class ArtistAdmin(admin.ModelAdmin):
     list_display = ("Idola", "Nama_Panggung", "Tanggal_Lahir", "Alamat", "Pesan_Moral",)
 
 admin.site.register(Artist, ArtistAdmin)
 admin.site.register(Idol)
-admin.site.register(Video)
