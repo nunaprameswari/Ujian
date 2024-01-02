d074143a4c22c2ceb946d53646cb1fd88606e2b9
nindiappc <nindi.nindia44@gmail.com>
2024-01-02 10:46:44 +0700

    perubahan CRUD

--- army/forms.py
+++ army/forms.py
@@ -1,1 +1,12 @@
 from django import forms
+from .models import ArmyMember
+
+class ArmyMemberForm(forms.ModelForm):
+    class Meta:
+        model = ArmyMember
+        fields = [
+            'first_name',
+            'last_name',
+            'instagram',
+            'twitter',
+        ]
