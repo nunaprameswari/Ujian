c4684592933805f943b57b15f9c58d07120d3e93
nindiappc <nindi.nindia44@gmail.com>
2024-01-02 10:26:29 +0700

    perubahan CRUD

--- army/admin.py
+++ army/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from .models import Artist, Idol, Image
+from .models import Artist, Idol, ArmyMember
 
 # Register your models here.
-@admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'photo', 'date']
 
 class ArtistAdmin(admin.ModelAdmin):
     list_display = ("Idola", "Nama_Panggung", "Tanggal_Lahir", "Alamat", "Pesan_Moral",)
 
 admin.site.register(Artist, ArtistAdmin)
 admin.site.register(Idol)
+admin.site.register(ArmyMember)
